da36d35a1e35e966081a050a52c33789d2eb1f18
kennethn23 <kennethng1337@gmail.com>
2023-10-03 09:22:57 -0400

    10/3/23

--- app.js
+++ app.js
@@ -30,19 +30,38 @@ addition(1, 2);
 // need to convert the string to a number
 let a = Number(prompt("enter a number"));
 let b = Number(prompt("enter another number"));
-addition(a, b); */
+addition(a, b); 
 
 function add(number1, number2) {
     // creates an output
     return (number1 + number2);
 }
 
-/* const sum = add(1, 2);
-console.log(sum); */
+const sum = add(1, 2);
+console.log(sum);
 
 function calc() {
     const sum = add(2, 5);
     console.log(sum);
 }
 
-calc();
+calc(); */
+
+
+
+/* const userName = ""
+const userHeight = 0
+const userWeight = 0 */
+
+function bmiCalculator(userHeight, userWeight) {
+    return ((userWeight / (userHeight * userHeight)) * 10000);
+}
+
+let userHeight = Number(prompt("Enter a height, in Centimeters"));
+let userWeight = Number(prompt("Enter a weight, in Kilograms"));
+const bmi = bmiCalculator(userHeight, userWeight);
+// console.log(bmi);
+
+const bmiDisplay = document.getElementById('bmi-display');
+const newContent = "Your BMI is: " + bmi;
+bmiDisplay.innerHTML = newContent;
